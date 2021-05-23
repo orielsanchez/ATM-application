37ce9ddc44a8f71a03493171a67119dcbd066e1d
Oriel Sanchez <orielbenitosanchez@gmail.com>
2021-05-23 12:36:10 -0700

    Added Javadoc comments to Account class

--- Server/src/main/java/edu/sdccd/cisc191/f/server/Account.java
+++ Server/src/main/java/edu/sdccd/cisc191/f/server/Account.java
@@ -8,33 +8,47 @@ import javax.persistence.Id;
 /**
  * This class represent a bank account at an ATM.
  *
+ * @author Oriel Sanchez
  */
 
-@Entity(name ="Account")
+@Entity(name = "Account")
 public class Account {
     @Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
-    private long ID; // This is generated in SQL's database at the moment
-    private long cardNumber; // This is the 16-digit card number
-    private String PIN;   // This is a 4-digit PIN
-    private double balance;        // represented in cents
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long ID;
+    private long cardNumber;
+    private String PIN;
+    private double balance;
 
 
-    protected Account() { }
+    protected Account() {
+    }
 
     /**
-     * Create an account object by three arguments
+     * Create an account object by three arguments.
+     * This constructor is used when the ID of the account
+     * has not been determined yet or is unknown.
+     *
      * @param cardNumber 16-digit card number
-     * @param PIN account PIN
-     * @param balance account balance
+     * @param PIN        account PIN
+     * @param balance    account balance
      */
-
     public Account(long cardNumber, String PIN, double balance) {
         this.cardNumber = cardNumber;
         this.PIN = PIN;
         this.balance = balance;
     }
 
+    /**
+     * Creates an account object by four arguments.
+     * This constructor is used when the ID of the account
+     * is known.
+     *
+     * @param ID         the id of the account
+     * @param cardNumber the 16-digit card number
+     * @param PIN        the account PIN
+     * @param balance    the account balance
+     */
     public Account(long ID, long cardNumber, String PIN, double balance) {
         this.ID = ID;
         this.cardNumber = cardNumber;
@@ -52,22 +66,50 @@ public class Account {
                 '}';
     }
 
+    /**
+     * Gets the PIN of the account
+     *
+     * @return The 4-digit PIN as a string
+     */
     public String getPIN() {
         return PIN;
     }
 
+    /**
+     * Gets the card number of the account
+     *
+     * @return the 16-digit card number as a long
+     */
     public long getCardNumber() {
         return cardNumber;
     }
 
+    /**
+     * Gets the balance of the account.
+     * This method is synchronized.
+     *
+     * @return the balance of the account
+     */
     public synchronized double getBalance() {
         return balance;
     }
 
+    /**
+     * Gets the ID of the account.
+     *
+     * @return the ID of the account
+     */
     public long getID() {
         return ID;
     }
 
+    /**
+     * Adds the amount argument to the balance if the amount is greater than or equal to zero.
+     * This method is synchronized.
+     *
+     * @param amount the double to be added to the balance
+     * @return true if the amount is added to the balance; false otherwise.
+     */
     public synchronized boolean deposit(double amount) {
         if (amount >= 0) {
             this.balance += amount;
@@ -77,6 +119,13 @@ public class Account {
         }
     }
 
+    /**
+     * Subtracts the amount argument from the balance if the balance is greater than the amount.
+     * This method is synchronized.
+     *
+     * @param amount the double to be subtracted from the balance.
+     * @return true if the amount is subtracted from the balance; false otherwise.
+     */
     public synchronized boolean withdraw(double amount) {
         if (balance < amount) {
             return false;
@@ -85,6 +134,4 @@ public class Account {
             return true;
         }
     }
-
-
 }
