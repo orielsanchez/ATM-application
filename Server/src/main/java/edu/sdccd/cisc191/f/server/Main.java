69077cc1f25431c5f61c73e787e85bb0911b8aad
Oriel Sanchez <orielbenitosanchez@gmail.com>
2021-05-23 18:14:08 -0700

    fixed Main

--- Server/src/main/java/edu/sdccd/cisc191/f/server/Main.java
+++ Server/src/main/java/edu/sdccd/cisc191/f/server/Main.java
@@ -2,8 +2,6 @@ package edu.sdccd.cisc191.f.server;
 
 import edu.sdccd.cisc191.f.server.account.AccountRepository;
 import edu.sdccd.cisc191.f.server.server.Server;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,20 +11,20 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 @EntityScan("edu.sdccd.cisc191.f")
 public class Main {
-    private static final Logger log = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) {
         SpringApplication.run(Main.class);
     }
-        @Bean
-        public CommandLineRunner startServer(AccountRepository accountRepository) {
-            return args -> {
-                Server server = new Server();
-                try {
-                    server.start(4444, accountRepository);
-                } catch(Exception e) {
-                    e.printStackTrace();
-                }
-            };
-        }
+
+    @Bean
+    public CommandLineRunner startServer(AccountRepository accountRepository) {
+        return args -> {
+            Server server = new Server();
+            try {
+                server.start(4444, accountRepository);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        };
     }
+}
