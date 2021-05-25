f7901f98cae993da3705b5838e5cf719882024a2
jonpl <jonathan_plata@ymail.com>
2021-05-24 19:16:56 -0700

    Added javadoc comments for depositRequest and depositResponse and edited comment in accountResponse

--- Common/src/main/java/edu/sdccd/cisc191/f/DepositRequest.java
+++ Common/src/main/java/edu/sdccd/cisc191/f/DepositRequest.java
@@ -3,7 +3,11 @@ package edu.sdccd.cisc191.f;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-
+/**
+ * his class holds the structure for starting the deposit requests within the socket.
+ *
+ * @author Oriel Sanchez
+ */
 public class DepositRequest {
     private RequestType requestType;
     private long cardNumber;
@@ -11,42 +15,87 @@ public class DepositRequest {
 
     @JsonIgnore
     private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    /**
+     * Creates the depositRequest object into json format to send within socket.
+     *
+     * @param  depositRequest depositRequest
+     * @return depositRequest as a string
+     */
     public static String toJSON(DepositRequest depositRequest) throws Exception {
         return objectMapper.writeValueAsString(depositRequest);
     }
 
+    /**
+     * Creates the depositRequest object into json format to send within socket.
+     *
+     * @param  input          reads an depositRequest formatted as a string
+     * @return converts the input string into an depositRequest
+     */
     public static DepositRequest fromJSON(String input) throws Exception {
         return objectMapper.readValue(input, DepositRequest.class);
     }
 
     protected DepositRequest(){}
 
+    /**
+     * Creates the depsitRequest object that accepts 3 parameters.
+     * This constructor is used to initialize the depositRequest object in order to create a deposit request.
+     *
+     * @param requestType    one of four account request types: Account, Deposit, Withdraw, Transfer
+     * @param cardNumber     cardnumber that acts as a bank account number
+     * @param depositAmount  amount to deposit into bank account
+     */
     public DepositRequest(RequestType requestType, long cardNumber, double depositAmount) {
         this.requestType = requestType;
         this.cardNumber = cardNumber;
         this.depositAmount = depositAmount;
     }
 
+    /**
+     * Gets the requestType
+     *
+     * @return The requestType as an object of requestType
+     */
     public RequestType getRequestType() {
         return requestType;
     }
 
+    /**
+     * Sets the requestType
+     */
     public void setRequestType(RequestType requestType) {
         this.requestType = requestType;
     }
 
+    /**
+     * Gets the card number from account
+     *
+     * @return the carbNumber as a long variable
+     */
     public long getCardNumber() {
         return cardNumber;
     }
 
+    /**
+     * Sets the cardNumber for accountRequest
+     */
     public void setCardNumber(long cardNumber) {
         this.cardNumber = cardNumber;
     }
 
+    /**
+     * Gets the deposit amount from depositRequest
+     *
+     * @return depositAmount as a double
+     */
     public double getDepositAmount() {
         return depositAmount;
     }
 
+    /**
+     * Sets the depositAmount for depositRequest
+     */
     public void setDepositAmount(double depositAmount) {
         this.depositAmount = depositAmount;
     }
--- Common/src/main/java/edu/sdccd/cisc191/f/DepositResponse.java
+++ Common/src/main/java/edu/sdccd/cisc191/f/DepositResponse.java
@@ -3,6 +3,11 @@ package edu.sdccd.cisc191.f;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+/**
+ * This class holds the structure to respond to depositRequests
+ *
+ * @author Oriel Sanchez
+ */
 public class DepositResponse {
     private long id;
     private long cardNumber;
@@ -11,10 +16,23 @@ public class DepositResponse {
 
     @JsonIgnore
     private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    /**
+     * Creates the depositResponse object into json format to send within socket.
+     *
+     * @param  depositResponse depositResponse
+     * @return The depositResponse as a string.
+     */
     public static String toJSON(DepositResponse depositResponse) throws Exception {
         return objectMapper.writeValueAsString(depositResponse);
     }
 
+    /**
+     * Creates the depositResponse object in json format from a string to send within socket.
+     *
+     * @param  input          reads an depositResponse formatted as a string
+     * @return converts the input string into an depositResponse
+     */
     public static DepositResponse fromJSON(String input) throws Exception {
         return objectMapper.readValue(input, DepositResponse.class);
     }
@@ -23,6 +41,15 @@ public class DepositResponse {
 
     }
 
+    /**
+     * Creates the depositResponse object that accepts 4 parameters.
+     * This constructor is used to initialize the depositResponse object in order to respond to deposit account requests.
+     *
+     * @param id          number to identify within database
+     * @param cardNumber  number that acts as a bank account number
+     * @param PIN         secure 4 digit pin
+     * @param balance     amount of money to deposit within account
+     */
     public DepositResponse(long id, long cardNumber, String PIN, double balance) {
         this.id = id;
         this.cardNumber = cardNumber;
@@ -30,34 +57,74 @@ public class DepositResponse {
         this.balance = balance;
     }
 
+    /**
+     * Gets the accountId
+     *
+     * @return The database id as a long variable
+     */
     public long getId() {
         return id;
     }
 
+    /**
+     * Sets the accountId
+     *
+     * @param id Accepts the id for the account
+     */
     public void setId(long id) {
         this.id = id;
     }
 
+    /**
+     * Gets the cardNumber associated with the account
+     *
+     * @return The cardNumber as a long variable
+     */
     public long getCardNumber() {
         return cardNumber;
     }
 
+    /**
+     * Sets the cardNumber associated with the account
+     *
+     * @param cardNumber Accepts the cardNumber for the account
+     */
     public void setCardNumber(long cardNumber) {
         this.cardNumber = cardNumber;
     }
 
+    /**
+     * Gets the PIN associated with the account
+     *
+     * @return The PIN as a String
+     */
     public String getPIN() {
         return PIN;
     }
 
+    /**
+     * Sets the PIN associated with the account
+     *
+     * @param PIN Accepts the PIN as a string
+     */
     public void setPIN(String PIN) {
         this.PIN = PIN;
     }
 
+    /**
+     * Gets the account balance
+     *
+     * @return The account balance as a double variable
+     */
     public double getBalance() {
         return balance;
     }
 
+    /**
+     * Sets the account balance
+     *
+     * @param balance Accepts the account balance as a double variable
+     */
     public void setBalance(double balance) {
         this.balance = balance;
     }
